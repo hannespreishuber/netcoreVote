53cbe79d7606f184ada001440ac52f0b6026deb1
hannes <hannesp@ppedv.de>
2015-11-14 17:06:47 +0100

    init

---
+++ src/netcoreVote/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Http;
+
+namespace netcoreVote
+{
+    public class Program
+    {
+        private static Timer timer;
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            timer = new Timer(_ => OnCallBack(), null, 1000 * 2, Timeout.Infinite);
+
+            Console.ReadLine();
+        }
+
+        private async void OnCallBack()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+            var json = await client.GetStringAsync(new Uri("http://ppedv.de/msts/votes.json"));
+            Console.WriteLine(json);
+            timer.Change(1000, 1000 * 2);
+        }
+    }
+}
